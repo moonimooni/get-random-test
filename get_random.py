5e9c526f01da7c28fdcefbd615131fa327745320
moonimooni <munhw1408@gmail.com>
2021-07-07 22:20:36 +0900

    Fix : 불필요한 데이터 타입 변환 제거

--- get_random.py
+++ get_random.py
@@ -21,12 +21,12 @@ def get_random(max_num):
 
   while True:
     # fill list of max_binary length with 0, 1 by random
-    random_bin_elements = [str(get_zero_or_one()) for _ in range(len(max_bin))]
+    random_bin_elements = [get_zero_or_one() for _ in range(len(max_bin))]
 
     # change binary to decimal and compare original max_num
     decimal = 0
     for i in range(len(random_bin_elements[::-1])):
-      if random_bin_elements[i] == "1":
+      if random_bin_elements[i] == 1:
         decimal += (2**i)
 
     if decimal <= max_num:
